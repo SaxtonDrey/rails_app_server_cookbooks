be45cc211a9834db11258bb52c68358235a9bcc6
SaxtonDrey <4phantomron@gmail.com>
2016-03-15 04:47:29 +0900

    change exec to script

--- jenkins_plugins/recipes/default.rb
+++ jenkins_plugins/recipes/default.rb
@@ -9,12 +9,12 @@
 
 JENKINS_URL = "http://localhost:8080"
 
-execute "install-plugins" do
+script "install_plugins" do
+  interpreter "bash"
   user "root"
   cmd = "wget -O jenkins-cli.jar #{JENKINS_URL}/jnlpJars/jenkins-cli.jar \n"
   node["jenkins_plugins"].each{|p|
     cmd += "java -jar jenkins-cli.jar -s #{JENKINS_URL} install-plugin #{p} \n"
   }
-  command cmd
-  action :run
+  code cmd
 end
